5e9766b877c59155d4269e8d9fa1ae81c317e55e
kishore-s8 <8kishore8@gmail.com>
2025-07-09 11:16:06 +0530

    Initial commit: deployService shared library

---
+++ vars/deployService.groovy
@@ -0,0 +1,26 @@
+def call(String imageName, String imageTag, String kubeconfigPath, String k8sPath) {
+    def fullImage = "${imageName}:${imageTag}"
+
+    stage('Build Docker Image') {
+        bat "docker build -t ${fullImage} ."
+    }
+
+    stage('Push Docker Image') {
+        withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+            bat """
+                docker login -u %DOCKER_USER% -p %DOCKER_PASS%
+                docker push ${fullImage}
+            """
+        }
+    }
+
+    stage('Deploy to Kubernetes') {
+        bat "kubectl --kubeconfig=\"${kubeconfigPath}\" apply -f \"${k8sPath}\""
+    }
+
+    stage('Verify Deployment') {
+        bat "kubectl --kubeconfig=\"${kubeconfigPath}\" get pods"
+        bat "kubectl --kubeconfig=\"${kubeconfigPath}\" get svc"
+    }
+}
+
