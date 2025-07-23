25be83eb6c5b78f55f01a09e973711b0361a0a36
kishore-s8 <159994215+kishore-s8@users.noreply.github.com>
2025-07-23 11:14:36 +0530

    Update deployService.groovy

--- vars/deployService.groovy
+++ vars/deployService.groovy
@@ -1,36 +1,38 @@
-def call(String imageName, String imageTag, String kubeconfigPath, String k8sPath, String giturl, String credentialsId) {
-    def fullImage = "${imageName}:${imageTag}"
+def call(String agentLabel, String imageName, String imageTag, String kubeconfigPath, String k8sPath, String giturl, String credentialsId) {
+    node(agentLabel) {
+        def fullImage = "${imageName}:${imageTag}"
 
-    stage('Checkout Code') {
-        checkout([
-            $class: 'GitSCM',
-            branches: [[name: '*/main']],
-            userRemoteConfigs: [[
-                credentialsId: "${credentialsId}",
-                url: "${giturl}"
-            ]]
-        ])
-    }
-
-    stage('Build Docker Image') {
-        bat "docker build -t ${fullImage} ."
-    }
-
-    stage('Push Docker Image') {
-        withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-            bat """
-                docker login -u %DOCKER_USER% -p %DOCKER_PASS%
-                docker push ${fullImage}
-            """
+        stage('Checkout Code') {
+            checkout([
+                $class: 'GitSCM',
+                branches: [[name: '*/main']],
+                userRemoteConfigs: [[
+                    credentialsId: "${credentialsId}",
+                    url: "${giturl}"
+                ]]
+            ])
         }
-    }
 
-    stage('Deploy to Kubernetes') {
-        bat "kubectl --kubeconfig=\"${kubeconfigPath}\" apply -f \"${k8sPath}\""
-    }
+        stage('Build Docker Image') {
+            bat "docker build -t ${fullImage} ."
+        }
 
-    stage('Verify Deployment') {
-        bat "kubectl --kubeconfig=\"${kubeconfigPath}\" get pods"
-        bat "kubectl --kubeconfig=\"${kubeconfigPath}\" get svc"
+        stage('Push Docker Image') {
+            withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+                bat """
+                    docker login -u %DOCKER_USER% -p %DOCKER_PASS%
+                    docker push ${fullImage}
+                """
+            }
+        }
+
+        stage('Deploy to Kubernetes') {
+            bat "kubectl --kubeconfig=\"${kubeconfigPath}\" apply -f \"${k8sPath}\""
+        }
+
+        stage('Verify Deployment') {
+            bat "kubectl --kubeconfig=\"${kubeconfigPath}\" get pods"
+            bat "kubectl --kubeconfig=\"${kubeconfigPath}\" get svc"
+        }
     }
 }
