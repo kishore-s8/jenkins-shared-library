02cb0790905d90ab5ba3af4721d96f81bcceea41
kishore-s8 <159994215+kishore-s8@users.noreply.github.com>
2025-07-25 14:27:43 +0530

    Update deploydocker.groovy

--- vars/deploydocker.groovy
+++ vars/deploydocker.groovy
@@ -1,1 +1,50 @@
 
+def call(String agentLabel, String imageName, String imageTag,
+         String appGitUrl, String credentialsId, String branch,
+         String dockerCredentialsId, String dockerRegistry) {
+
+    node(agentLabel) {
+
+        stage('Checkout Application Code') {
+            dir('app') {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: branch]],
+                    userRemoteConfigs: [[
+                        credentialsId: credentialsId,
+                        url: appGitUrl
+                    ]]
+                ])
+            }
+        }
+
+        def fullImage = "${dockerRegistry}/${imageName}:${imageTag}"
+
+        stage('Build Docker Image') {
+            dir('app') {
+                bat "docker build -t ${fullImage} ."
+            }
+        }
+
+        stage('Push Docker Image') {
+            withCredentials([usernamePassword(credentialsId: dockerCredentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+                bat """
+                    docker login -u %DOCKER_USER% -p %DOCKER_PASS% ${dockerRegistry}
+                    docker push ${fullImage}
+                """
+            }
+        }
+
+        stage('Run Container') {
+            bat """
+                docker stop ${imageName} || echo "No existing container"
+                docker rm ${imageName} || echo "No container to remove"
+                docker run -d --name ${imageName} -p 8080:8080 ${fullImage}
+            """
+        }
+
+        stage('Verify Container') {
+            bat "docker ps -a"
+        }
+    }
+}
