aef87660ed0b6771a136189317dac5f19b2a5e20
kishore-s8 <159994215+kishore-s8@users.noreply.github.com>
2025-07-29 10:16:00 +0530

    Update deploypubliimages.groovy

--- vars/deploypubliimages.groovy
+++ vars/deploypubliimages.groovy
@@ -1,21 +1,41 @@
-def call(String agentLabel, String imageName, String imageTag) {
-    node('') {
-        def fullImage = "${imageName}:${imageTag}"
+// vars/deployMultipleImages.groovy
+def call(List<Map> images, String registry = '') {
+    images.each { image ->
+        def name = image.name
+        def tag = image.tag ?: 'latest'
 
-        stage('Pull Public Image') {
-            bat "docker pull ${fullImage}"
-        }
+        echo "Deploying image: ${name}:${tag}"
 
-        stage('Run Container') {
-            bat """
-                docker stop ${imageName} || echo "No such container"
-                docker rm ${imageName} || echo "No container to remove"
-                docker run -d --restart unless-stopped --name ${imageName} -p 8081:80 ${fullImage}
-            """
-        }
-
-        stage('Verify Container') {
-            bat "docker ps -a"
-        }
+        bat """
+            docker pull ${name}:${tag}
+            docker tag ${name}:${tag} ${registry}/${name}:${tag}
+            docker push ${registry}/${name}:${tag}
+        """
+         stage('Verify Container') {
+             bat "docker ps -a"
+         }
     }
 }
+
+
+// def call(String agentLabel, String imageName, String imageTag) {
+//     node('') {
+//         def fullImage = "${imageName}:${imageTag}"
+
+//         stage('Pull Public Image') {
+//             bat "docker pull ${fullImage}"
+//         }
+
+//         stage('Run Container') {
+//             bat """
+//                 docker stop ${imageName} || echo "No such container"
+//                 docker rm ${imageName} || echo "No container to remove"
+//                 docker run -d --restart unless-stopped --name ${imageName} -p 8081:80 ${fullImage}
+//             """
+//         }
+
+//         stage('Verify Container') {
+//             bat "docker ps -a"
+//         }
+//     }
+// }
