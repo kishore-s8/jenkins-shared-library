edf9674820a367144a2e2b5ee7fc8b8b711ca642
kishore-s8 <159994215+kishore-s8@users.noreply.github.com>
2025-07-31 10:54:41 +0530

    Update deploydocker.groovy

--- vars/deploydocker.groovy
+++ vars/deploydocker.groovy
@@ -1,7 +1,13 @@
-
-def call(String agentLabel, String imageName, String imageTag,
-         String appGitUrl, String credentialsId, String branch,
-         String dockerCredentialsId, String dockerRegistry) {
+// vars/buildAndDeploy.groovy
+def call(Map config = [:]) {
+    def agentLabel = config.get('agentLabel', '')
+    def imageName = config.get('imageName', 'web-calculator')
+    def imageTag = config.get('imageTag', 'v1.0')
+    def appGitUrl = config.get('appGitUrl', 'https://github.com/kishore-s8/simple-calculator.git')
+    def credentialsId = config.get('credentialsId', '')
+    def branch = config.get('branch', 'main')
+    def dockerCredentialsId = config.get('dockerCredentialsId', '')
+    def dockerRegistry = config.get('dockerRegistry', 'docker.io/8kishore8')
 
     node(agentLabel) {
 
